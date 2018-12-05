f95f9212ef10add63f9f850c90879cda0b33171f
zhengzhixiong <zhengzhixiong@baijiahulian.com>
2018-12-05 12:21:58 +0800

    mybatis hello world 工程项目搭建

---
+++ java/测试数据库脚本.sql
@@ -0,0 +1,4 @@
+-- 用户表
+CREATE TABLE t_mybatis_user(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), id_no INT, age INT);
+INSERT INTO t_mybatis_user(name, id_no, age) VALUES('zhangsan', 18);
+INSERT INTO t_mybatis_user(name, id_no, age) VALUES('lisi', 17);
